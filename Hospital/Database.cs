e65084a5a263edba47c74c1c70493c0e126ede63
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-05 03:55:26 +0300

    1

---
+++ Hospital/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Database
+    {
+        private List<Patient> _patients = new List<Patient>();
+
+        public Database() 
+        {
+            AddPatients();
+        }
+
+        private void AddPatients() 
+        {
+            _patients.Add(new Patient("Дуров Эдуард Самадович","Ангина",35));
+            _patients.Add(new Patient("Гонинов Вячеслав Американович","Ветрянка",35));
+            _patients.Add(new Patient("Громов Аркадий Олегович", "Атит", 37));
+            _patients.Add(new Patient("Колокольчиков Игорь Аркадьевич", "Орв", 40));
+            _patients.Add(new Patient("Клонов Клон Клонович", "Сердечная недостаточность", 41));
+            _patients.Add(new Patient("Проныров Андрей Игоревич", "Хитрость", 21));
+            _patients.Add(new Patient("Дворников Дворник Степанович", "Старость", 80));
+            _patients.Add(new Patient("Прищиков Олег Олегович", "Алергия", 78));
+            _patients.Add(new Patient("Зубнов Антон Артемонович", "Стамотит", 51));
+            _patients.Add(new Patient("Грунов Юрий Дмитриевич", "Насморк", 19));
+        }
+    }
+}
