c13e5672670e72f588a8d17bbeca307eae44c07d
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-05 03:43:58 +0300

    1

---
+++ Hospital/Patient.cs
@@ -0,0 +1,19 @@
+﻿namespace Hospital
+{
+    internal class Patient
+    {
+        public Patient(string fullName, string disease, int age) 
+        {
+            FullName = fullName;
+
+            Disease = disease;
+
+            Age = age;
+        }
+
+        public string FullName { get; private set; }
+        public string Disease{ get; private set; }
+
+        public int Age { get; private set; }
+    }
+}
